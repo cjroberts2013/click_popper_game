b5cb5839528a425aaef3e2d0ae8c882d877339bf
Charles Roberts <cjroberts2013@gmail.com>
2019-09-19 12:28:46 -0500

    more progress mad on the game

---
+++ script.js
@@ -0,0 +1,52 @@
+const playArea = {};
+const player = {};
+let gameObj;
+
+playArea.stats = document.querySelector(".stats");
+playArea.main = document.querySelector(".main");
+playArea.game = document.querySelector(".game");
+playArea.btn = Array.from(document.querySelectorAll(".btn"));
+playArea.page = Array.from(document.querySelectorAll(".page"));
+document.addEventListener("DOMContentLoaded", getData);
+
+playArea.btn.forEach(function(item) {
+  item.addEventListener("click", handleBtn);
+});
+
+function getData() {
+  playArea.main.classList.add("visable");
+  fetch("https://api.myjson.com/bins/gungm")
+    .then(function(rep) {
+      return rep.json();
+    })
+    .then(function(data) {
+      gameObj = data.data;
+      buildBoard();
+    });
+}
+
+function buildBoard() {
+  let rows = 4;
+  let cols = 4;
+  let cnt = 0;
+  playArea.game.getElementsByClassName.width = cols * 100 + cols * 2;
+  playArea.game.getElementsByClassName.margin = "auto";
+  for (let y = 0; y < cols; y++) {
+    let divMain = document.createElement("div");
+    divMain.setAttribute("class", "row");
+    for (let x = 0; x < rows; x++) {}
+  }
+}
+
+function handleBtn(e) {
+  if (e.target.classList.contains("newGame")) {
+    startGame();
+  }
+}
+
+function startGame() {
+  player.score = 0;
+  player.items = 3;
+  playArea.main.classList.remove("visable");
+  playArea.game.classList.add("visable");
+}
